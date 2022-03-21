53a947b127e3f5d1bf08ea0fc8c75d39905d2a32
sontv8 <sontv8@fpt.edu.vn>
2022-03-21 15:09:11 +0700

    category create

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import homeRoute from './routes/home'
 import productRoute from "./routes/products"
+import categoryRoute from './routes/category'
 import { checkAuth } from './middlewares/checkAuth';
 const app = express();
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(homeRoute);
 app.use("/api",checkAuth,productRoute);
+app.use("/api",categoryRoute)
 
 mongoose.connect('mongodb://localhost:27017/we16308');
 
